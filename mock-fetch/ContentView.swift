4da27bb6eed94a99f1fc32874f4694b5b1ccfce8
Eric Martinez <ericlmartinez@gmail.com>
2019-09-25 09:53:35 -0700

    Initial Commit

---
+++ mock-fetch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mock-fetch
+//
+//  Created by Eric Martinez on 9/25/19.
+//  Copyright © 2019 emobile. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
